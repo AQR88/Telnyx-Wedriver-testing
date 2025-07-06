35fbb0c8ae9738a538b7b74d63c0fb13adec4a42
AQR88 <timahaua@gmail.com>
2025-07-07 01:12:32 +0300

    updating

--- wdio.firefox.conf.js
+++ wdio.firefox.conf.js
@@ -2,5 +2,5 @@ import { config as base } from "./wdio.conf.js";
 export const config = {
   ...base,
   capabilities: [{ browserName: "firefox" }],
-  services: ["geckodriver"],
+  services: ["selenium-standalone"],
 };
